85806537ad9c67a229b989f7c6827f7db2097cfc
Benjamin Rose <benr242@localhost.localdomain>
2019-10-09 17:12:29 -0400

    dummy band

--- src/Controller/ORMController.php
+++ src/Controller/ORMController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Artist;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ORMController extends AbstractController
 {
     /**
-     * @Route("/o/r/m", name="o_r_m")
+     * @Route("/orm", name="orm")
      */
     public function index()
     {
         return $this->render('orm/index.html.twig', [
             'controller_name' => 'ORMController',
         ]);
+    }
+
+    /**
+     * @Route("/orm/add", name="orm-add")
+     */
+    public function addDummy(EntityManager $em)
+    {
+        $nin = new Artist();
+        $nin->setName("Nine Inch Nails");
+        $em->persist($nin);
+        $em->flush();
     }
 }
