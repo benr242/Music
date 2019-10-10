dad337dfa93db637585852f5cc49be7d316dd069
Benjamin Rose <benr242@localhost.localdomain>
2019-10-09 23:18:58 -0400

    check

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,11 +12,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-
-
         $nin = new Artist();
         $nin->setName("Nine Inch Nails");
         $manager->persist($nin);
@@ -32,62 +27,69 @@ class AppFixtures extends Fixture
         $song->setNumber(1);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("Terrible Lie");
         $song->setAlbum($phm);
         $song->setNumber(2);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("Down In It");
         $song->setAlbum($phm);
         $song->setNumber(3);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("Sancttified");
         $song->setAlbum($phm);
         $song->setNumber(4);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("Something I Can Never Have");
         $song->setAlbum($phm);
         $song->setNumber(5);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("Kinda I Want To");
         $song->setAlbum($phm);
         $song->setNumber(6);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("Sin");
         $song->setAlbum($phm);
         $song->setNumber(7);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("That's What I Get");
         $song->setAlbum($phm);
         $song->setNumber(8);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("The Onlf Time");
         $song->setAlbum($phm);
         $song->setNumber(9);
         $song->setLength(500);
         $manager->persist($song);
+
         $song = new Song();
         $song->setName("Ringfinger");
         $song->setAlbum($phm);
         $song->setNumber(10);
         $song->setLength(500);
         $manager->persist($song);
-
-
 
         $manager->flush();
     }
