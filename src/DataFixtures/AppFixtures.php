fcc5bbf54ee39bafffda96887e04fbd5b01e82e8
Benjamin Rose <benr242@localhost.localdomain>
2019-10-09 21:13:02 -0400

    added fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Album;
+use App\Entity\Artist;
+use App\Entity\Song;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -11,6 +14,27 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+
+
+        $nin = new Artist();
+        $nin->setName("Nine Inch Nails");
+        $manager->persist($nin);
+
+        $phm = new Album();
+        $phm->setArtist($nin);
+        $phm->setName("Pretty Hate Machine");
+        $manager->persist($phm);
+
+        $hlh = new Song();
+        $hlh->setName("Head Like a Hole");
+        $hlh->setAlbum($phm);
+        $hlh->setNumber(1);
+        $hlh->setLength(500);
+        $manager->persist($hlh);
+
+
+
 
         $manager->flush();
     }
