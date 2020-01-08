b30154403dc449be57215a382db3b7ef5bb1aa7f
Benjamin Rose <benr242@localhost.localdomain>
2020-01-08 10:18:42 -0500

    check

--- src/Controller/ORMController.php
+++ src/Controller/ORMController.php
@@ -369,6 +369,15 @@ class ORMController extends AbstractController
     }
 
     /**
+     * @Route("/orm/removeSong/{songId}
+     *     name="removeSong")
+     */
+    public function removeSon(int $songId, EntityManagerInterface $em, SongRepository $sr)
+    {
+        
+    }
+
+    /**
      * @Route("/orm/success",
      *      name="success")
      */
